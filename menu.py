4ab03a4d6f3d47d5d6c70cd2555caf2fcbd0504b
PieAba <82049121+PieAba@users.noreply.github.com>
2021-05-03 12:45:46 +0200

    Update menu.py
    initial menu.py

--- menu.py
+++ menu.py
@@ -1,1 +1,28 @@
+# --------------------------------------------------------------
+#  
+#  Pietro Abati
+#  Version: 1.0.0
+#  Last Updated: 2021 - 05 - 03 
+# --------------------------------------------------------------
 
+# --------------------------------------------------------------
+#  GLOBAL IMPORTS ::::::::::::::::::::::::::::::::::::::::::::::
+# --------------------------------------------------------------
+
+import nuke
+import platform
+
+# Define where .nuke directory is on each OS's network.
+Win_Dir = 'C:\Users\Pietro\.nuke'
+MacOSX_Dir = '/Users/Pietro/.nuke'
+Linux_Dir = '/home/Pietro/.nuke'
+
+# Automatically set global directory
+if platform.system() == "Windows":
+	dir = Win_Dir
+elif platform.system() == "Darwin":
+	dir = MacOSX_Dir
+elif platform.system() == "Linux":
+	dir = Linux_Dir
+else:
+	dir = None
