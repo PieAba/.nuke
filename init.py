0416ad2aa0317f2db3f18922153ffd5b88169934
PieAba <82049121+PieAba@users.noreply.github.com>
2021-05-03 12:44:42 +0200

    Update init.py

--- init.py
+++ init.py
@@ -1,1 +1,12 @@
+# --------------------------------------------------------------
+#  init.py
+#  Version: 1.0.0
+#  Last Updated: April 9th, 2021
+# --------------------------------------------------------------
+
+# ----- DEFINE CUSTOM FOLDER STRUCTURE -------------------------
+
+nuke.pluginAddPath('./gizmos')
+nuke.pluginAddPath('./python')
+
 
